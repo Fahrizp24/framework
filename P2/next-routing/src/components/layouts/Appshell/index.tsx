57c5c3b1e9f066fc82aa4cccddf13fc8a76403d5
Fahrizp24 <Fpradian24@gmail.com>
2026-02-20 02:07:42 +0700

    P2_6. Membuat Layout Global (App Shell)

---
+++ P2/next-routing/src/components/layouts/Appshell/index.tsx
@@ -0,0 +1,20 @@
+import Navbar from "../navbar";
+
+type AppShellProps = {
+  children: React.ReactNode;
+}
+
+const AppShell = (props: AppShellProps) => {
+  const { children } = props;
+  return (
+    <main>
+      <Navbar />
+      {children}
+      <div>
+        <p>footer</p>
+      </div>
+    </main>
+  );
+};
+
+export default AppShell;
