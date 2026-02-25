e524753af82b0b17b7e16018be01dca2a5594fc3
Fahrizp24 <Fpradian24@gmail.com>
2026-02-25 21:46:04 +0700

    P4_4. Conditional Rendering Navbar (Tanpa Navbar di Login)

--- P4/next-routing/src/components/layouts/Appshell/index.tsx
+++ P4/next-routing/src/components/layouts/Appshell/index.tsx
@@ -1,5 +1,7 @@
+import { useRouter } from "next/router";
 import Navbar from "../navbar";
-import Footer from "../footer";
+
+const disableNavbar = ['/auth/login', '/auth/register'];
 
 type AppShellProps = {
   children: React.ReactNode;
@@ -7,13 +9,12 @@ type AppShellProps = {
 
 const AppShell = (props: AppShellProps) => {
   const { children } = props;
+  const { pathname } = useRouter();
+
   return (
     <main>
-      <Navbar />
+      {!disableNavbar.includes(pathname) && <Navbar />}
       {children}
-      <div>
-        <Footer />
-      </div>
     </main>
   );
 };
