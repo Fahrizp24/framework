82363c49ae068ea6ebf4709832f4694a9f6c696d
Fahrizp24 <Fpradian24@gmail.com>
2026-03-04 00:08:43 +0700

    P8_ 2 – Implementasi getServerSideProps pada server.tsx

--- P8/next-routing/src/pages/produk/server.tsx
+++ P8/next-routing/src/pages/produk/server.tsx
@@ -1,12 +1,34 @@
 import TampilanProduk from "../../views/produk";
 
-const HalamanProdukServer = () => {
+type ProductType = {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+  category: string;
+};
+
+const HalamanProdukServer = (props: { products: ProductType[] }) => {
+  const { products } = props;
+  
   return (
     <div>
       <h1>Halaman Produk Server</h1>
-      <TampilanProduk products={[]} />
+      <TampilanProduk products={products} />
     </div>
   );
 };
 
 export default HalamanProdukServer;
+
+// Fungsi ini dipanggil setiap request halaman
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/produk");
+  const response = await res.json();
+  
+  return {
+    props: {
+      products: response.data, // Pastikan ada nilai default/handling jika gagal
+    },
+  };
+}
