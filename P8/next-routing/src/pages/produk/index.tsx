9a1be6eca17623c21d660a9b460502a77bb44657
Fahrizp24 <Fpradian24@gmail.com>
2026-03-04 00:08:12 +0700

    P8_1 – Setup Halaman SSR

---
+++ P8/next-routing/src/pages/produk/index.tsx
@@ -0,0 +1,15 @@
+import TampilanProduk from "../../views/produk";
+import useSWR from "swr";
+import fetcher from "../utils/swr/fetcher";
+
+const kategori = () => {
+  const { data, error, isLoading } = useSWR("/api/produk", fetcher);
+
+  return (
+    <div>
+      <TampilanProduk products={isLoading ? [] : data?.data} />
+    </div>
+  );
+};
+
+export default kategori;
