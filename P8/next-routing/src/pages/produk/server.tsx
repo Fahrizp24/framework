579012da73c74d007a3b2fdc62218ab3264b82f5
Fahrizp24 <Fpradian24@gmail.com>
2026-03-04 00:12:56 +0700

    P8_3 – Refactor Type ( produk type )

--- P8/next-routing/src/pages/produk/server.tsx
+++ P8/next-routing/src/pages/produk/server.tsx
@@ -1,12 +1,5 @@
 import TampilanProduk from "../../views/produk";
-
-type ProductType = {
-  id: string;
-  name: string;
-  price: number;
-  image: string;
-  category: string;
-};
+import { ProductType } from "../types/Product.type";
 
 const HalamanProdukServer = (props: { products: ProductType[] }) => {
   const { products } = props;
@@ -21,14 +14,13 @@ const HalamanProdukServer = (props: { products: ProductType[] }) => {
 
 export default HalamanProdukServer;
 
-// Fungsi ini dipanggil setiap request halaman
 export async function getServerSideProps() {
   const res = await fetch("http://localhost:3000/api/produk");
   const response = await res.json();
   
   return {
     props: {
-      products: response.data, // Pastikan ada nilai default/handling jika gagal
+      products: response.data,
     },
   };
 }
