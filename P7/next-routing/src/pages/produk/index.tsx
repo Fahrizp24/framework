721d4214b6c4ce76241e924ee0906d3316eb7e86
Fahrizp24 <Fpradian24@gmail.com>
2026-03-03 23:22:36 +0700

    P7_5 – Implementasi SWR

--- P7/next-routing/src/pages/produk/index.tsx
+++ P7/next-routing/src/pages/produk/index.tsx
@@ -1,23 +1,13 @@
-import { useEffect, useState } from "react";
 import TampilanProduk from "../../views/produk";
+import useSWR from "swr";
+import fetcher from "../utils/swr/fetcher";
 
 const kategori = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    fetch("/api/produk")
-      .then((response) => response.json())
-      .then((responsedata) => {
-        setProducts(responsedata.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching produk:", error);
-      });
-  }, []);
+  const { data, error, isLoading } = useSWR("/api/produk", fetcher);
 
   return (
     <div>
-      <TampilanProduk products={products} />
+      <TampilanProduk products={isLoading ? [] : data?.data} />
     </div>
   );
 };
