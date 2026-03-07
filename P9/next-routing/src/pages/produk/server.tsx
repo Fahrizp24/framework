dce8256ed10d6c08bb1df0b8b27f3bb893a77ea4
Fahrizp24 <Fpradian24@gmail.com>
2026-03-07 23:00:03 +0700

    P9_1 – Setup Halaman Static

---
+++ P9/next-routing/src/pages/produk/server.tsx
@@ -0,0 +1,26 @@
+import TampilanProduk from "../../views/produk";
+import { ProductType } from "../types/Product.type";
+
+const HalamanProdukServer = (props: { products: ProductType[] }) => {
+  const { products } = props;
+  
+  return (
+    <div>
+      <h1>Halaman Produk Server</h1>
+      <TampilanProduk products={products} />
+    </div>
+  );
+};
+
+export default HalamanProdukServer;
+
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/produk");
+  const response = await res.json();
+  
+  return {
+    props: {
+      products: response.data,
+    },
+  };
+}
