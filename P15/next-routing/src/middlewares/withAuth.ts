260d03befb68d6475c487684fb4de3dbe6757d21
Fahrizp24 <Fpradian24@gmail.com>
2026-03-31 21:37:03 +0700

    P15_ 5 – Callback URL Logic

--- P15/next-routing/src/middlewares/withAuth.ts
+++ P15/next-routing/src/middlewares/withAuth.ts
@@ -1,6 +1,8 @@
 import { getToken } from "next-auth/jwt";
 import { NextFetchEvent, NextMiddleware, NextRequest, NextResponse } from "next/server";
 
+const hanyaAdmin = ["/admin"];
+
 export default function withAuth(
   middleware: NextMiddleware,
   requireAuth: string[] = []
@@ -13,13 +15,15 @@ export default function withAuth(
         req,
         secret: process.env.NEXTAUTH_SECRET,
       });
-
       if (!token) {
-        const loginUrl = new URL("/", req.url); // Redirect ke halaman login bawaan NextAuth
+        const loginUrl = new URL("/auth/login", req.url); // Redirect ke halaman login bawaan NextAuth
+        loginUrl.searchParams.set("callbackUrl", encodeURI(req.url)); // untuk menyimpan url yang ingin diakses
         return NextResponse.redirect(loginUrl);
       }
+      if (token.role !== "admin" && hanyaAdmin.includes(pathname)) {
+        return NextResponse.redirect(new URL("/", req.url)); // Redirect ke halaman utama jika role bukan admin
+      }
     }
-
     return middleware(req, next);
   };
 }
