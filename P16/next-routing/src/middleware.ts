cc9047d1af9778e7df36c9d4154d826ba29757af
Fahrizp24 <Fpradian24@gmail.com>
2026-04-01 20:15:34 +0700

    P16_4 – Buat OAuth Credentials

---
+++ P16/next-routing/src/middleware.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import withAuth from "./middlewares/withAuth";
+
+export function mainMiddleware(req: NextRequest) {
+  const res = NextResponse.next();
+  return res;
+}
+
+// Memanggil wrapper withAuth dan memasukkan "/profile" ke dalam array requireAuth
+export default withAuth(mainMiddleware, ["/profile", "/admin"]);
